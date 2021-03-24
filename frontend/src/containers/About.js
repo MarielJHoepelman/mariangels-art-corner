efdd347891987aefd39228e0f31ea95a421b502a
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-03-24 16:28:33 -0400

    [Frontend] Creates About container.
    - Container connects to redux for handling state changes. 
    - Maps the state to the props. 
    - Dispatchs the fetcher action to the props. 
    - Returns About component and sends the props to component.

---
+++ frontend/src/containers/About.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetcher } from "../actions/fetcher";
+import { About as AboutComponent } from "../components/About";
+
+class About extends Component {
+  componentDidMount() {
+    this.props.fetcher();
+  }
+
+  render() {
+    return (
+      <AboutComponent data={this.props.data} loading={this.props.loading} />
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { data: state.data, loading: state.loading };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetcher: () => dispatch(fetcher()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(About);
