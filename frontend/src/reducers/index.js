06f74900ea94419a2da5ed1ab4e1f878511f1e5c
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-03-24 16:00:09 -0400

    [Frontend] Creates reducer function.
    -Reducer function handles loading data and data loaded actions and 
    updates the state or return the unchanged state depending on the action 
    received.

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,3 +1,18 @@
-export default function reducers(state = {}, action) {
-  return state;
+export default function reducers(state = { data: {}, loading: false }, action) {
+  switch (action.type) {
+    case "LOADING_DATA":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "DATA_LOADED":
+      console.log(action);
+      return {
+        ...state,
+        data: action.data,
+        loading: false,
+      };
+    default:
+      return state;
+  }
 }
