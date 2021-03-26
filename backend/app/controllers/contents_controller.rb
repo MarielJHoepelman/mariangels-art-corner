e60dfc0acac292bfca0b09b135dba9b323c4cea2
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-03-26 16:10:26 -0400

    [Backend] Refactors Contents controller.
    - Refactors show action to send model’s attachment urls to the server.
    - Configures images private function that maps through model’s 
    attachments and returns the urls.

--- backend/app/controllers/contents_controller.rb
+++ backend/app/controllers/contents_controller.rb
@@ -2,7 +2,22 @@ class ContentsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def show
-    @content = Content.find_by_page_name(params[:page_name])
-    render json: @content.to_json
+    content = Content.find_by_page_name(params[:page_name])
+
+    render json: {
+      page_name: content.page_name,
+      title: content.title,
+      body: content.body,
+      images: images(content)
+    }
+
+    # render json: content.as_json(include: { images: images(content) })
+
+  end
+
+  private
+
+  def images(content)
+    content.images.map { |e| url_for(e)  }
   end
 end
