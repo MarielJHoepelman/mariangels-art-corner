b84b57f2f606eb9eeefe7bfa74a7f17ad43147a8
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-12 17:11:10 -0400

    [Backend] Refactors the Contents controller show action so it renders the instance attachments urls.

--- backend/app/controllers/contents_controller.rb
+++ backend/app/controllers/contents_controller.rb
@@ -3,16 +3,15 @@ class ContentsController < ApplicationController
 
   def show
     content = Content.find_by_page_name(params[:page_name])
-
-    render json: {
-      page_name: content.page_name,
-      title: content.title,
-      body: content.body,
-      images: images(content)
-    }
-
+    # render json: {
+    #   page_name: content.page_name,
+    #   title: content.title,
+    #   body: content.body,
+    #   images: content.attachments
+    # }
+    # render json: content.to_json
     # render json: content.as_json(include: { images: images(content) })
-
+    render :json => content.to_json(:methods => [:image_file])
   end
 
   private
