39042d47e501c2fdbe833c41725c008ab5e7f6fe
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-12 17:24:37 -0400

    [Backend] Refactors Contents admin.
    - Adds image_attributes and destroy as permitted parameters. 
    - Adds image field to the activeadmin edit functionality that allows for 
    uploading images. 
    - Adds a checkbox that allows deleting an existing image. 
    - Image_tag shows image thumbnail if exists.

--- backend/app/admin/contents.rb
+++ backend/app/admin/contents.rb
@@ -10,7 +10,7 @@ ActiveAdmin.register Content do
   # or
   #
   permit_params do
-    permitted = [:page_name, :title, :body, :image_source, images: []]
+    permitted = [:page_name, :title, :body, :image_source,  :image, image_attributes: [:_destroy], images: []]
     # permitted << :other if params[:action] == 'create' && current_user.admin?
     permitted
   end
@@ -20,9 +20,17 @@ ActiveAdmin.register Content do
       f.input :page_name
       f.input :title
       f.input :body
-      f.input :images, as: :file, input_html: { multiple: true }
-      f.object.images.each do |at|
-        span image_tag(at.variant(resize: "250x250!"))
+      # f.input :images, as: :file, input_html: { multiple: true }
+      # f.object.images.each do |at|
+      #   span image_tag(at.variant(resize: "250x250!"))
+      # end
+      f.input :image, required: true, as: :file
+
+      if f.object.image.present?
+        span image_tag(f.object.image.url(:thumb))
+        f.semantic_fields_for :image_attributes do |image_fields|
+          image_fields.input :_destroy, as: :boolean, label: 'Delete?'
+        end
       end
     end
 
