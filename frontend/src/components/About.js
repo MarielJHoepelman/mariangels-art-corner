bd441d3f9ddc5f714792a03fa61ae0b8683ad394
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-12 16:59:48 -0400

    [Frontend] Refactors About component.
    - Conditionally creates an image tag if the props contain an image.

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -7,6 +7,9 @@ export const About = ({ data, loading }) => {
     <div>
       <h1>{data.title}</h1>
       <p>{data.body}</p>
+      {data?.image_file?.large && (
+        <img src={data.image_file.large} alt="profile" />
+      )}
     </div>
   );
 };
