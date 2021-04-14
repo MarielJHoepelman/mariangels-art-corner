4b9bf285429e783a4230a1b3eb2b8b34412b48c9
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-14 16:26:38 -0400

    [Backend] Migrations.
    - Adds content-type column to contents table.
    - Removes image-source from contents table.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_205320) do
+ActiveRecord::Schema.define(version: 2021_04_14_194656) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -51,13 +51,13 @@ ActiveRecord::Schema.define(version: 2021_04_08_205320) do
     t.string "page_name"
     t.string "title"
     t.text "body"
-    t.string "image_source"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_file_name"
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.string "content_type"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
