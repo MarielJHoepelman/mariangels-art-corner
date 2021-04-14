ce178edf7726b67bccf8432c6b2ce1e88d1e3cff
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-14 16:46:48 -0400

    [Backend] Refactors Contents model.
    - Configure PaperClip relationship with model.   
    - Adds validation for images content type and styles (sizes). 
    - Creates a method image_file to parse the PaperClip images styles. 
    - Creates image_attributes setter function to allow for deletion of 
    images.

--- backend/app/models/content.rb
+++ backend/app/models/content.rb
@@ -1,3 +1,20 @@
 class Content < ApplicationRecord
   validates :page_name, uniqueness: true
+  has_attached_file :image, styles: { large: "600x600>", medium: "350x350>", thumb: "100x100>" }
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
+  def image_file
+    if self.image.present?
+      {
+        original: ApplicationController.helpers.asset_url(self.image.url(:original)),
+        thumb: ApplicationController.helpers.asset_url(self.image.url(:thumb)),
+        medium: ApplicationController.helpers.asset_url(self.image.url(:medium)),
+        large: ApplicationController.helpers.asset_url(self.image.url(:large)),
+      }
+    end
+  end
+
+  def image_attributes=(attributes)
+    image.clear if has_destroy_flag?(attributes) && !image.dirty?
+  end
 end
