2bd09810710b9c541bf7d62619aeb53e4527009b
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-20 16:23:39 -0400

    [Frontend] Home Component.
    - Changes default export to named export so it can be imported with 
    alias in Home container. 
    - Configures component so it displays the content data from props or to 
    display “loading” while promise is resolved.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,24 @@
-import React from "react";
-
-const Home = () => {
-  return (
+export const Home = ({ data, loading }) => {
+  return loading ? (
+    <div> loading... </div>
+  ) : (
     <div>
-      <h1>Tita's Art Corner</h1>
+      <h1>{data.title}</h1>
+      <p>{data.body}</p>
+      {data?.image_file?.large && (
+        <img src={data.image_file.thumb} alt="bubble" />
+      )}
+      <h1>PORTFOLIO</h1>
+      <div>
+        {data?.display_art &&
+          data.display_art.map((art) => (
+            <img
+              key={art.page_name}
+              src={art.image_file.medium}
+              alt={art.title}
+            />
+          ))}
+      </div>
     </div>
   );
 };
-
-export default Home;
