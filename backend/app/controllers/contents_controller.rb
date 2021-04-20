2434c656c3023d3d902caac2a9a50cabb3f252ec
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-20 13:35:51 -0400

    [Controller] Authenticity token
    - Move authenticity token to application_controller controller so it
    covers all controllers.

--- backend/app/controllers/contents_controller.rb
+++ backend/app/controllers/contents_controller.rb
@@ -1,6 +1,4 @@
 class ContentsController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
   def show
     content = Content.find_by_page_name(params[:page_name])
     # render json: {
