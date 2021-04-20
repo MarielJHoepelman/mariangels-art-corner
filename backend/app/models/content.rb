b14594e31655efe02642091398080104efdb075a
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-20 16:29:29 -0400

    [Backend] Content model.
    - Refactors image-file method to return object with image(s) if image(s) 
    are present. 
    - Creates display-art method that returns objects of all content type of 
    “art” and sets it to include the page-name, title and image-file 
    attributes.

--- backend/app/models/content.rb
+++ backend/app/models/content.rb
@@ -4,17 +4,28 @@ class Content < ApplicationRecord
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
   def image_file
-    if self.image.present?
+    if image.present?
       {
-        original: ApplicationController.helpers.asset_url(self.image.url(:original)),
-        thumb: ApplicationController.helpers.asset_url(self.image.url(:thumb)),
-        medium: ApplicationController.helpers.asset_url(self.image.url(:medium)),
-        large: ApplicationController.helpers.asset_url(self.image.url(:large)),
+        original: ApplicationController.helpers.asset_url(image.url(:original)),
+        thumb: ApplicationController.helpers.asset_url(image.url(:thumb)),
+        medium: ApplicationController.helpers.asset_url(image.url(:medium)),
+        large: ApplicationController.helpers.asset_url(image.url(:large)),
       }
     end
   end
 
   def image_attributes=(attributes)
     image.clear if has_destroy_flag?(attributes) && !image.dirty?
+  end
+
+  def display_art
+    art_content = Content.where(content_type: "art")
+    art_content.map do |art|
+      {
+        page_name: art.page_name,
+        title: art.title,
+        image_file: art.image_file,
+      }
+    end
   end
 end
