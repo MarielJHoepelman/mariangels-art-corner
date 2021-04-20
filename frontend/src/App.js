432b542c7aa2d920588f1d8f4761659f3fb3e79e
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-20 15:56:26 -0400

    [Frontend] App.
    - Imports Home container instead of component. 
    - Imports Header component and ContactUs container.
    - Adds contact-us route path to Router.
    - Removes dead code from imports.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
-import Home from "./components/Home";
+import Home from "./containers/Home";
 import About from "./containers/About";
+import Header from "./components/Header";
+import ContactUs from "./containers/ContactUs";
 
 function App(props) {
   return (
     <Router>
       <div>
+        <Header />
         <NavBar />
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
+        <Route exact path="/contact-us" component={ContactUs} />
       </div>
     </Router>
   );
