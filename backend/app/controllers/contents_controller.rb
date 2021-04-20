955ae797e6c63c302566dbbe0b6fe5821bf4bb6c
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-20 13:49:03 -0400

    [Backend] Removes developer comments and unused private function.

--- backend/app/controllers/contents_controller.rb
+++ backend/app/controllers/contents_controller.rb
@@ -1,20 +1,6 @@
 class ContentsController < ApplicationController
   def show
     content = Content.find_by_page_name(params[:page_name])
-    # render json: {
-    #   page_name: content.page_name,
-    #   title: content.title,
-    #   body: content.body,
-    #   images: content.attachments
-    # }
-    # render json: content.to_json
-    # render json: content.as_json(include: { images: images(content) })
-    render :json => content.to_json(:methods => [:image_file])
-  end
-
-  private
-
-  def images(content)
-    content.images.map { |e| url_for(e)  }
+    render :json => content.to_json(:methods => [:image_file, :display_art])
   end
 end
