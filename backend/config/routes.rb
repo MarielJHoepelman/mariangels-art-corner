ad0fe6f2bb424234e195bd53c919f90908d218e4
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-20 15:57:43 -0400

    [Backend] Adds contacts resources to Rails routes.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :contents, param: :page_name
+  resources :contacts, only: :create
 end
