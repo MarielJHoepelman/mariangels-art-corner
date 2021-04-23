09f5dd4fd30f99cc85c9307a7908d2325a53ed2b
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-23 13:41:29 -0400

    [Frontend] Creates Frequently Asked Questions Container.
    - Connects the container to the Redux store.  
    - Maps the state to the props.  
    - Dispatches Fetcher to the Frequently Asked Questions component.

---
+++ frontend/src/containers/FrequentlyAskedQuestions.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetcher } from "../actions/fetcher";
+import { FrequentlyAskedQuestions as FrequentlyAskedQuestionsComponent } from "../components/FrequentlyAskedQuestions";
+
+class FrequentlyAskedQuestions extends Component {
+  componentDidMount() {
+    this.props.fetcher();
+  }
+
+  render() {
+    return (
+      <FrequentlyAskedQuestionsComponent
+        data={this.props.data}
+        loading={this.props.loading}
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { data: state.data, loading: state.loading };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetcher: () =>
+      dispatch(fetcher("contents/frequently-asked-questions", "GET")),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FrequentlyAskedQuestions);
