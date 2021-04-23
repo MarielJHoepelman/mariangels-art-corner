c4b033094d03894812203b44b727a147e8d60eb4
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-23 12:10:55 -0400

    [Frontend] Adds styling to About component.
    - Imports styled from styled components. 
    - Sets the content text and image to the center of the screen. 
    - Refactors the bio section to use dangerouslySetInnerHTML attribute to 
    display text as structured in admin.

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,13 +1,25 @@
+import styled from "styled-components";
+
+const Bio = styled.div`
+  text-align: center;
+  width: 60%;
+  margin-left: auto;
+  margin-right: auto;
+  line-height: 30px;
+`;
+
 export const About = ({ data, loading }) => {
   return loading ? (
     <div> loading... </div>
   ) : (
     <div>
-      <h1>{data.title}</h1>
-      <p>{data.body}</p>
-      {data?.image_file?.large && (
-        <img src={data.image_file.large} alt="profile" />
-      )}
+      <Bio>
+        <h1>{data.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: data.body }} />
+        {data?.image_file?.large && (
+          <img src={data.image_file.large} alt="profile" />
+        )}
+      </Bio>
     </div>
   );
 };
