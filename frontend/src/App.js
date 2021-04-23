31a6003f9525baec75b0d49e312921e0b6e5f3f9
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-23 15:53:42 -0400

    [Frontend] Updates App.js.
    - Imports Footer and FrequentlyAskedQuestions components to App.  
    - Imports Styled from styled components. 
    - Creates a BodyContainer section that wraps all routes but Footer so 
    the footer remains fixed and separate from contents of the window. 
    - Adds bottom margin to BodyContainer to detach it from the footer.
    - Deletes unused App.css import.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
 import NavBar from "./components/NavBar";
 import Home from "./containers/Home";
 import About from "./containers/About";
 import Header from "./components/Header";
 import ContactUs from "./containers/ContactUs";
+import Footer from "./components/Footer";
+import FrequentlyAskedQuestions from "./containers/FrequentlyAskedQuestions";
+import styled from "styled-components";
+
+const BodyContainer = styled.div`
+  margin-bottom: 100px;
+`;
 
 function App(props) {
   return (
     <Router>
-      <div>
+      <BodyContainer>
         <Header />
         <NavBar />
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/contact-us" component={ContactUs} />
-      </div>
+        <Route exact path="/faq" component={FrequentlyAskedQuestions} />
+      </BodyContainer>
+      <Footer />
     </Router>
   );
 }
