64bdba529172ab84451674a21eb73a98c540703b
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-23 13:35:28 -0400

    [Frontend] Creates Frequently Asked Questions Component.
    - Imports Styled from styled components.
    - Sets the content text and image to the center of the screen. 
    - Sets bio section to use dangerouslySetInnerHTML attribute to display 
    text coming from props as structured in admin.

---
+++ frontend/src/components/FrequentlyAskedQuestions.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const FaqStyled = styled.div`
+  text-align: center;
+`;
+
+export const FrequentlyAskedQuestions = ({ data, loading }) => {
+  return (
+    <FaqStyled>
+      <h1>{data.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: data.body }} />
+    </FaqStyled>
+  );
+};
