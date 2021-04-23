ded06427cc3b7833d557a669f7878cf85ddebec8
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-23 15:56:32 -0400

    [Frontend] Updates Home Component.
    - Imports Styled from styled components.  
    - Adds styling to the top div of the component so text is displayed at 
    the center of the window.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,23 +1,34 @@
+import styled from "styled-components";
+
+const TopDiv = styled.div`
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
 export const Home = ({ data, loading }) => {
   return loading ? (
     <div> loading... </div>
   ) : (
     <div>
-      <h1>{data.title}</h1>
-      <p>{data.body}</p>
-      {data?.image_file?.large && (
+      <TopDiv>
+        <h1>{data.title}</h1>
+        <h3>{data.body}</h3>
+        {/*data?.image_file?.large && (
         <img src={data.image_file.thumb} alt="bubble" />
-      )}
-      <h1>PORTFOLIO</h1>
+      )*/}
+      </TopDiv>
       <div>
-        {data?.display_art &&
-          data.display_art.map((art) => (
-            <img
-              key={art.page_name}
-              src={art.image_file.medium}
-              alt={art.title}
-            />
-          ))}
+        <div>
+          {data?.display_art &&
+            data.display_art.map((art) => (
+              <img
+                key={art.page_name}
+                src={art.image_file.medium}
+                alt={art.title}
+              />
+            ))}
+        </div>
       </div>
     </div>
   );
