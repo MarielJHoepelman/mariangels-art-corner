7b18a6387acc8f31e97ed7045b25448e72e5a13a
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-23 13:53:33 -0400

    [Frontend] Updates Footer Component.
    - Imports Styled from styled components. 
    - Sets the color theme of the block to match logo gradient. 
    - Sets the block to be fixed at the bottom of the browser window.
    - Displays copyright notice.
    - Imports and displays Instagram logo within the block. 
    - Links Instagram logo to the site owner’s Instagram account.

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,41 @@
-import React from "react";
+import styled from "styled-components";
+import instagram from "../images/instagram.png";
 
-function Footer() {
+const StickyFooter = styled.footer`
+  padding: 10px 0px;
+  background: linear-gradient(to right, #ff627e, #41d5fa);
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Copyright = styled.p`
+  padding-left: 20px;
+`;
+
+const InstagramLogo = styled.a`
+  padding-right: 20px;
+  img {
+    height: 50px;
+    width: 50px;
+  }
+`;
+
+const Footer = () => {
   return (
-    <div>
-      <h1>This is footer</h1>
-    </div>
+    <StickyFooter>
+      <Copyright>2021 Copyright: created by Mariel Hoepelman</Copyright>
+      <InstagramLogo
+        href="https://www.instagram.com/angelswirlette/"
+        target="blank"
+      >
+        <img src={instagram} alt="Instagram Logo Gradient" />
+      </InstagramLogo>
+    </StickyFooter>
   );
-}
+};
 
 export default Footer;
