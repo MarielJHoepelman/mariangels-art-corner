733c7a7b0bbe2dc40cb9917f0d6820d41ded1cbf
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-23 13:45:51 -0400

    [Frontend] Adds styling to Contact Us Component.
    - Imports Styled from styled components. 
    - Sets the color theme 
    - Sets the text and the form to be in the center of the screen.

--- frontend/src/components/ContactUs.js
+++ frontend/src/components/ContactUs.js
@@ -1,6 +1,72 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
+const TopDiv = styled.div`
+  margin-top: 20px;
+  margin-bottom: 30px;
+  text-align: center;
+`;
+
+const StyledForm = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  width: 420px;
+  max-width: 100%;
+`;
+
+const StyledLabel = styled.label`
+  display: block;
+  font-size: 18px;
+  line-height: 1.4em;
+  margin-bottom: 8px;
+  margin-top: 8px;
+`;
+
+const StyledInput = styled.input`
+  font-size: 18px;
+  line-height: 18px;
+  background: #fff;
+  color: #000;
+  border: 1px solid #d9d9d9;
+  padding: 0.8em 15px;
+  margin: 0;
+  vertical-align: middle;
+  width: 100%;
+  border-radius: 5px;
+  box-sizing: border-box;
+`;
+
+const StyledTextArea = styled.textarea`
+  line-height: 18px;
+  height: 120px;
+  color: #000;
+  border: 1px solid #d9d9d9;
+  padding: 0.8em 15px;
+  margin: 0;
+  vertical-align: middle;
+  width: 100%;
+  border-radius: 0;
+  box-sizing: border-box;
+`;
+
+const StyledButton = styled.button`
+  background: #ff627e;
+  border: 1px solid #f4a8a8;
+  color: #fff;
+  font-size: 15px;
+  line-height: 1em;
+  height: auto;
+  margin: 0;
+  cursor: pointer;
+  padding: 1.2em 25px 1.2em;
+  vertical-align: middle;
+  text-align: center;
+  border-radius: 5px;
+  display: inline-block;
+  letter-spacing: 0.08em;
+  margin-top: 8px;
+`;
+
 export class ContactUs extends Component {
   constructor(props) {
     super(props);
@@ -28,42 +94,46 @@ export class ContactUs extends Component {
   render() {
     return (
       <div>
-        <h1>CONTACT US</h1>
-        <p>Send us a message!</p>
-        <p>
-          Check out our FAQ page to see if the answer to your question is
-          already there :)
-        </p>
+        <TopDiv>
+          <h1>CONTACT US</h1>
+          <p>Send us a message!</p>
+          <p>
+            Check out our FAQ page to see if the answer to your question is
+            already there :)
+          </p>
+        </TopDiv>
 
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input
-              name="name"
-              type="text"
-              value={this.state.name}
-              onChange={this.handleOnChange}
-            />
-          </label>
-          <label>
-            Email:
-            <input
-              name="email"
-              type="email"
-              value={this.state.email}
-              onChange={this.handleOnChange}
-            />
-          </label>
-          <label>
-            Message:
-            <textarea
-              name="message"
-              onChange={this.handleOnChange}
-              value={this.state.message}
-            />
-          </label>
-          <input type="submit" value="Send" />
-        </form>
+        <div>
+          <StyledForm onSubmit={this.handleSubmit}>
+            <div>
+              <StyledLabel>Name:</StyledLabel>
+              <StyledInput
+                name="name"
+                type="text"
+                value={this.state.name}
+                onChange={this.handleOnChange}
+              />
+            </div>
+            <div>
+              <StyledLabel>Email:</StyledLabel>
+              <StyledInput
+                name="email"
+                type="email"
+                value={this.state.email}
+                onChange={this.handleOnChange}
+              />
+            </div>
+            <div>
+              <StyledLabel>Message:</StyledLabel>
+              <StyledTextArea
+                name="message"
+                onChange={this.handleOnChange}
+                value={this.state.message}
+              />
+            </div>
+            <StyledButton type="submit">SEND</StyledButton>
+          </StyledForm>
+        </div>
       </div>
     );
   }
