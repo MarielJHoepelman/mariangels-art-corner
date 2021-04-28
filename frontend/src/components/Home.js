e1fda548bb1b156a7fea1f14f396ab5b9b07a1de
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-28 13:40:45 -0400

    [Frontend] Home Component.
    - Imports Link from react router.
    - Sets font and right padding of TopDiv styled component. 
    - Creates the ImageContainer, TopDivWrapper and StyledLink styled 
    components and sets their color, font, measurements and location within 
    the view.  
    - Call data.art_randomizer instead of data.display_art to get five 
    random content type art from the backend.  
    - Adds link see all that links to the /art url.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,40 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const TopDiv = styled.div`
+  padding-right: 100px;
+  font-family: Montserrat;
+`;
+
+const ImageContainer = styled.div`
+  padding-right: 100px;
+`;
+
+const TopDivWrapper = styled.div`
+  background: #ff627e;
+  height: 500px;
+  margin-bottom: 30px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const StyledLink = styled(Link)`
+  background: #ff627e;
+  border: 1px solid #f4a8a8;
+  color: #fff;
+  font-size: 15px;
+  line-height: 1em;
+  height: auto;
+  margin: 0;
+  cursor: pointer;
+  padding: 1.2em 25px 1.2em;
+  vertical-align: middle;
   text-align: center;
-  margin-top: 20px;
-  margin-bottom: 20px;
+  border-radius: 5px;
+  display: inline-block;
+  letter-spacing: 0.08em;
+  margin-top: 8px;
 `;
 
 export const Home = ({ data, loading }) => {
@@ -11,24 +42,31 @@ export const Home = ({ data, loading }) => {
     <div> loading... </div>
   ) : (
     <div>
-      <TopDiv>
-        <h1>{data.title}</h1>
-        <h3>{data.body}</h3>
-        {/*data?.image_file?.large && (
-        <img src={data.image_file.thumb} alt="bubble" />
-      )*/}
-      </TopDiv>
+      <TopDivWrapper>
+        <TopDiv>
+          <h1>{data.title}</h1>
+          <h3>{data.body}</h3>
+        </TopDiv>
+        <ImageContainer>
+          {data?.image_file?.large && (
+            <img src={data.image_file.medium} alt="profile" />
+          )}
+        </ImageContainer>
+      </TopDivWrapper>
       <div>
         <div>
-          {data?.display_art &&
-            data.display_art.map((art) => (
-              <img
-                key={art.page_name}
-                src={art.image_file.medium}
-                alt={art.title}
-              />
+          {data?.art_randomizer &&
+            data.art_randomizer.map((art) => (
+              <Link to={`/art/${art.page_name}`}>
+                <img
+                  key={art.page_name}
+                  src={art.image_file.square}
+                  alt={art.title}
+                />
+              </Link>
             ))}
         </div>
+        <StyledLink to="/art">See all</StyledLink>
       </div>
     </div>
   );
