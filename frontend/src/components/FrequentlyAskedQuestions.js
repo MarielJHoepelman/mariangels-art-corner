5c7ac024a2023fe39185d3a68c0e85c6a2ff1cb6
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-04-28 13:30:40 -0400

    [Frontend] FrequentlyAskedQuestions Component.
    - Adds Montserrat font family to component styles.

--- frontend/src/components/FrequentlyAskedQuestions.js
+++ frontend/src/components/FrequentlyAskedQuestions.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const FaqStyled = styled.div`
   text-align: center;
+  font-family: Montserrat;
 `;
 
 export const FrequentlyAskedQuestions = ({ data, loading }) => {
