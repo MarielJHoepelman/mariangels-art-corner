ebc9175d2f6904a2e593e9d4957f22898acc283f
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-05-10 12:58:59 -0400

    [Frontend] Updates Shop component.
    - Adds styling to the component, such as font family, font color, 
    margins and hover animation.     
    - Makes that the products are presented in rows of 4 when the display is 
    full screen.

--- frontend/src/components/Shop.js
+++ frontend/src/components/Shop.js
@@ -1,24 +1,47 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-export const Shop = ({ data, loading }) => {
+export const ProductCardLink = styled(Link)`
+  text-align: center;
+  text-decoration: none;
+  font-family: Montserrat;
+  color: #ff627e;
+  font-weight: bold;
+  :hover {
+    color: #41d5fa;
+  }
+`;
+
+export const ProductCardsContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  flex-wrap: wrap;
+  max-width: 1450px;
+  margin: 0 auto;
+`;
+
+export const Shop = ({ products, loading }) => {
   return loading ? (
     <div> loading... </div>
   ) : (
-    <div>
-      {data.length > 0 &&
-        data.map((product) => (
-          <Link to={`/shop/${product.page_name}`} key={product.page_name}>
+    <ProductCardsContainer>
+      {products.length > 0 &&
+        products.map((product) => (
+          <ProductCardLink
+            to={`/shop/${product.page_name}`}
+            key={product.page_name}
+          >
             <div>
               <img
                 src={product.product_image.square}
                 alt={product.product_name}
               />
               <div>{product.product_name}</div>
-              <div>{product.price}</div>
+              <div>${product.price}</div>
             </div>
-          </Link>
+          </ProductCardLink>
         ))}
-    </div>
+    </ProductCardsContainer>
   );
 };
