69a3b25a2f562298f05569d4d363826fa768ef83
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-05-10 19:32:05 -0400

    [Backend] Updates Products controller.
    - Index action renders all instances of products.
    - Show action finds a product by name and renders the instance of the 
    product.
    - Private method render_json receives a data argument and converts the 
    data to json and renders it along with the model method product_image.

--- backend/app/controllers/products_controller.rb
+++ backend/app/controllers/products_controller.rb
@@ -1,7 +1,16 @@
 class ProductsController < ApplicationController
   def index
     products = Product.all
+    render_json(products)
+  end
 
-    render :json => products.to_json(:methods => [:product_image])
+  def show
+    product = Product.find_by(page_name: params[:page_name])
+    render_json(product)
+  end
+
+  private
+  def render_json(data)
+    render :json => data.to_json(:methods => [:product_image])
   end
 end
