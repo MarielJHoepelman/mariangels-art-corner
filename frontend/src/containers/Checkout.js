ed4d79c69aa7f4723be4a18465c8c9b27d4ed8f9
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-05-12 12:46:51 -0400

    [Frontend] Creates Checkout container.
    - Connects to the Redux store. 
    - Imports CardElement, Elements and ElementsConsumer components from 
    Stripe.
    - stripePromise loads Stripe package functionality using the publishable 
    key. 
    - InjectedCheckoutForm wraps the Checkout form component with the Stripe 
    elements consumer. 
    - Maps the shopping cart state to the props and passes the props to the 
    component.

---
+++ frontend/src/containers/Checkout.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Checkout as CheckoutComponent } from "../components/Checkout";
+import {
+  CardElement,
+  Elements,
+  ElementsConsumer,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
+
+const InjectedCheckoutForm = () => (
+  <ElementsConsumer>
+    {({ stripe, elements }) => {
+      return (
+        stripe &&
+        elements && <CheckoutComponent stripe={stripe} elements={elements} />
+      );
+    }}
+  </ElementsConsumer>
+);
+
+class Checkout extends Component {
+  render() {
+    return (
+      <Elements stripe={stripePromise}>
+        <InjectedCheckoutForm />;
+      </Elements>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { shoppingCart: state.shopping_cart };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
