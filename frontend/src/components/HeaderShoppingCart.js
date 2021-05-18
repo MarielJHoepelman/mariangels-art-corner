51a60e199a4bb4d18dab53f3f5430fd5ecf594cb
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-05-18 17:45:11 -0400

    [Frontend] Updates HeaderShoppingCart.js.
    - Imports and displays the shopping bag icon from images. 
    - Adds styles to icon.

--- frontend/src/components/HeaderShoppingCart.js
+++ frontend/src/components/HeaderShoppingCart.js
@@ -1,9 +1,16 @@
-import styled from "styled-components";
+import shopping_bag_icon from "../images/shopping_bag_icon.svg";
+import { Link } from "react-router-dom";
+import { ShoppingBagWrapper, QtyWrapper } from "../styles";
 
 export const HeaderShoppingCart = ({ shoppingCart }) => {
   const productsTotalQty = shoppingCart.reduce((acc, current) => {
     return (acc += current.qty);
   }, 0);
 
-  return <div>{productsTotalQty}</div>;
+  return (
+    <ShoppingBagWrapper to="/cart">
+      <img src={shopping_bag_icon} alt="shopping bag icon" />
+      <QtyWrapper>{productsTotalQty}</QtyWrapper>
+    </ShoppingBagWrapper>
+  );
 };
