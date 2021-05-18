33b51f3c25c5161037332953735d5ef8884b73a3
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-05-18 17:56:01 -0400

    [Backend] Creates Orders and Order Items tables.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_211322) do
+ActiveRecord::Schema.define(version: 2021_05_14_213500) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -58,6 +58,22 @@ ActiveRecord::Schema.define(version: 2021_05_03_211322) do
     t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.string "content_type"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "product_id"
+    t.decimal "price", precision: 4, scale: 2
+    t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "products", force: :cascade do |t|
