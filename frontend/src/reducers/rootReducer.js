af1352cbc1b5e0ef458d0b535d0aad3d6088f192
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-05-24 13:13:34 -0400

    [Frontend] Updates rootReducer.
    - Adds user and order keys to the root reducer.

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -2,11 +2,15 @@ import { combineReducers } from "redux";
 import contentsReducer from "./contentsReducer";
 import productsReducer from "./productsReducer";
 import shoppingCartReducer from "./shoppingCartReducer";
+import userReducer from "./userReducer";
+import orderReducer from "./orderReducer";
 
 const rootReducer = combineReducers({
   content: contentsReducer,
   products: productsReducer,
   shopping_cart: shoppingCartReducer,
+  user: userReducer,
+  order: orderReducer,
 });
 
 export default rootReducer;
