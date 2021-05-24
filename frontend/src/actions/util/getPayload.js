fe11c368b61655c779a0b2e2b4809becfc114cd6
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-05-24 16:24:28 -0400

    [Frontend] Updates getPayload utility function.
    - Instantiates jwt_token constant and sets it to equal the value of the 
    jwt_token key in the local storage. 
    - Adds Authorization attribute to the header equal to the value of  
    jwt_token variable.

--- frontend/src/actions/util/getPayload.js
+++ frontend/src/actions/util/getPayload.js
@@ -1,8 +1,13 @@
-export const getPayload = (method, body) => ({
-  method: method,
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
-  body: body ? JSON.stringify(body) : null,
-});
+export const getPayload = (method, body) => {
+  const jwt_token = localStorage.getItem("jwt_token");
+
+  return {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: jwt_token,
+    },
+    body: body ? JSON.stringify(body) : null,
+  };
+};
