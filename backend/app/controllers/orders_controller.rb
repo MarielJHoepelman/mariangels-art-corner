6cac2ada5d0a5e0212efe69a062a89062f70bc14
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-05-24 17:14:35 -0400

    [Backend] Updates Orders controller.
    - Configures the create action so it creates a new order with the params 
    and render a json with the order data. 
    - Configures the index action so it renders a json with all the orders 
    that the user_id matches the current_user id. 
    - Creates a private order_params method for the required and permitted 
    params.

--- backend/app/controllers/orders_controller.rb
+++ backend/app/controllers/orders_controller.rb
@@ -1,6 +1,29 @@
 class OrdersController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def create
+    # 1. create the order with the params. Order.create(....)
+    # 2. delete the cart for the useR OR have an status boolen completed /true /false
+    # binding.pry
+    order = Order.new(user_id: order_params[:user_id], total: order_params[:total])
+
+    order_params[:order_items].each do |item|
+      order.order_items << OrderItem.create(item)
+    end
+
+    order.save
+
+    render json: order
   end
+
+  def index
+    orders = Order.find_by(user_id: current_user.id)
+    render json: orders
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:user_id, :total, :order_items=> [:product_id, :price, :quantity])
+  end
+
 end
