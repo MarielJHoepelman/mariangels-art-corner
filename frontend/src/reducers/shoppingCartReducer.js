37080d0eafb38ae159de49f1de4ae4aac66e95d4
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-05-24 13:12:03 -0400

    [Frontend] Updates shoppingCartReducer.
    - Updates the reducer to save the updated state of the cart in the local 
    storage to maintain local persistence of the data and preventing losing 
    the cart data if the browser is refreshed.

--- frontend/src/reducers/shoppingCartReducer.js
+++ frontend/src/reducers/shoppingCartReducer.js
@@ -4,16 +4,35 @@ export default function shoppingCartReducer(state = [], action) {
       const product = state.find((prod) => prod.id === action.product.id);
 
       if (product) {
-        product.qty += action.product.qty;
+        product.quantity += action.product.quantity;
+        window.localStorage.setItem(
+          "shopping_cart",
+          JSON.stringify([...state])
+        );
         return [...state];
       } else {
+        window.localStorage.setItem(
+          "shopping_cart",
+          JSON.stringify([...state, action.product])
+        );
         return [...state, action.product];
       }
+
     case "REMOVE_PRODUCT_FROM_SHOPPING_CART":
       const products = state.filter(
         (product) => product.id !== action.product.id
       );
+      window.localStorage.setItem("shopping_cart", JSON.stringify(products));
       return products;
+
+    case "FETCH_SHOPPING_CART":
+      const shoppingCartFromLocalStorage = window.localStorage.getItem(
+        "shopping_cart"
+      );
+      return JSON.parse(shoppingCartFromLocalStorage) || [];
+    case "CLEAR_SHOPPING_CART":
+      window.localStorage.removeItem("shopping_cart");
+      return [];
     default:
       return state;
   }
