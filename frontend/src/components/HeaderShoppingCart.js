ae972ee6328fb8042250109a546dbbe574a41aa5
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-05-24 16:02:09 -0400

    [Frontend] Updates HeaderShoppingCart component.
    - Imports React component. 
    - Changes from functional component to class component. 
    - Calls componentDidMount and within it, calls the cartFetcher function 
    that fetches the state of the cart from the local storage when the 
    component is mounted. 
    - Configures productsTotalQuantity function that calculates the total 
    value of the cart. 
    - Displays the products images and total value of the cart in the view.

--- frontend/src/components/HeaderShoppingCart.js
+++ frontend/src/components/HeaderShoppingCart.js
@@ -1,16 +1,29 @@
+import React, { Component } from "react";
 import shopping_bag_icon from "../images/shopping_bag_icon.svg";
 import { Link } from "react-router-dom";
 import { ShoppingBagWrapper, QtyWrapper } from "../styles";
 
-export const HeaderShoppingCart = ({ shoppingCart }) => {
-  const productsTotalQty = shoppingCart.reduce((acc, current) => {
-    return (acc += current.qty);
-  }, 0);
+export class HeaderShoppingCart extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
 
-  return (
-    <ShoppingBagWrapper to="/cart">
-      <img src={shopping_bag_icon} alt="shopping bag icon" />
-      <QtyWrapper>{productsTotalQty}</QtyWrapper>
-    </ShoppingBagWrapper>
-  );
-};
+  componentDidMount() {
+    this.props.cartFetcher();
+  }
+
+  productsTotalQuantity = () =>
+    this.props.shoppingCart.reduce((acc, current) => {
+      return (acc += current.quantity);
+    }, 0);
+
+  render() {
+    return (
+      <ShoppingBagWrapper to="/cart">
+        <img src={shopping_bag_icon} alt="shopping bag icon" />
+        <QtyWrapper>{this.productsTotalQuantity()}</QtyWrapper>
+      </ShoppingBagWrapper>
+    );
+  }
+}
