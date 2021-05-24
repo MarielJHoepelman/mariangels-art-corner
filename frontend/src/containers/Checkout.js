2b5ea2f74c0c4ada5367d5b02678bdc4e040a813
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-05-24 13:52:21 -0400

    [Frontend] Updates Checkout container.
    - Removes Stripe elements imports, stripePromise and injected form. 
    - Refactors mapStateToProps to also return user and order objects. 
    - Updates mapDispatchToProps function to dispatch clearCart to the 
    reducer and to dispatch the account action that fetches the backend to 
    get the user account data.

--- frontend/src/containers/Checkout.js
+++ frontend/src/containers/Checkout.js
@@ -1,54 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { submitOrder } from "../actions/submitOrder";
-import { Checkout as CheckoutComponent } from "../components/Checkout";
-import {
-  CardElement,
-  Elements,
-  ElementsConsumer,
-} from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-
-const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
-
-const InjectedCheckoutForm = ({ shoppingCart, submitOrder }) => (
-  <ElementsConsumer>
-    {({ stripe, elements }) => {
-      return (
-        stripe &&
-        elements && (
-          <CheckoutComponent
-            stripe={stripe}
-            elements={elements}
-            shoppingCart={shoppingCart}
-            submitOrder={submitOrder}
-          />
-        )
-      );
-    }}
-  </ElementsConsumer>
-);
-
-class Checkout extends Component {
-  render() {
-    return (
-      <Elements stripe={stripePromise}>
-        <InjectedCheckoutForm
-          shoppingCart={this.props.shoppingCart}
-          submitOrder={this.props.submitOrder}
-        />
-      </Elements>
-    );
-  }
-}
+import { Checkout } from "../components/Checkout";
+import { account } from "../actions/account";
 
 const mapStateToProps = (state) => {
-  return { shoppingCart: state.shopping_cart };
+  return {
+    shoppingCart: state.shopping_cart,
+    user: state.user,
+    order: state.order,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     submitOrder: (order) => dispatch(submitOrder("orders", "POST", order)),
+    clearCart: () => dispatch({ type: "CLEAR_SHOPPING_CART" }),
+    account: () => dispatch(account("account", "GET")),
   };
 };
 
