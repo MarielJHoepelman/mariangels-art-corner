85b2336f47358cd3501dfff0800a3da93b32a3ab
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-05-24 16:55:19 -0400

    [Backend] Modifies schema of Users and Orders tables.
    - Adds name and last name columns to Users table. 
    - Changes column type from integer to decimal in the total column of the 
    Orders table.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_213500) do
+ActiveRecord::Schema.define(version: 2021_05_23_221828) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 2021_05_14_213500) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "total"
+    t.decimal "total"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -97,6 +97,8 @@ ActiveRecord::Schema.define(version: 2021_05_14_213500) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
