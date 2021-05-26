df2a0307430394de49707711fc58c21ec5a9fc5e
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-05-26 19:07:35 -0400

    [Backend] Orders Controller
    - Removes comments
    - Wraps return in render_resource function to account for errors
    - adds show route to return a single order

--- backend/app/controllers/orders_controller.rb
+++ backend/app/controllers/orders_controller.rb
@@ -2,26 +2,28 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    # 1. create the order with the params. Order.create(....)
-    # 2. delete the cart for the useR OR have an status boolen completed /true /false
-    # binding.pry
     order = Order.new(user_id: order_params[:user_id], total: order_params[:total])
 
     order_params[:order_items].each do |item|
       order.order_items << OrderItem.create(item)
     end
-
+    
     order.save
-
-    render json: order
+    render_resource(order)
   end
 
   def index
-    orders = Order.find_by(user_id: current_user.id)
+    orders = Order.where(user_id: current_user.id)
     render json: orders
   end
 
+  def show
+    order = Order.find(params[:id])
+    render :json => order.to_json(:methods => [:order_items_products])
+  end
+
   private
+
   def order_params
     params.require(:order).permit(:user_id, :total, :order_items=> [:product_id, :price, :quantity])
   end
