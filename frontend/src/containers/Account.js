869212a9dd36a0231f372df50ef7673924acce36
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-05-27 16:18:26 -0400

    [Frontend] Updates Account Container.
    - Sets componentDidMount to dispatch fetch user and fetch user orders. 
    - Imports Account Component. 
    - Adds orders to the definition of state. 
    - Adds ordersFetcher action that dispatches orderFetcher function with 
    page name and method as arguments

--- frontend/src/containers/Account.js
+++ frontend/src/containers/Account.js
@@ -1,17 +1,35 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Account } from "../components/Account";
+import { Account as AccountComponent } from "../components/Account";
 import { logout } from "../actions/logout";
 import { account } from "../actions/account";
+import { ordersFetcher } from "../actions/ordersFetcher";
+
+class Account extends Component {
+  componentDidMount() {
+    this.props.account().then((user) => {
+      if (!user) {
+        this.props.history.push("/login");
+      } else {
+        this.props.ordersFetcher();
+      }
+    });
+  }
+
+  render() {
+    return <AccountComponent {...this.props} />;
+  }
+}
 
 const mapStateToProps = (state) => {
-  return { user: state.user };
+  return { user: state.user, orders: state.orders };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     logout: () => dispatch(logout("logout", "DELETE")),
     account: () => dispatch(account("account", "GET")),
+    ordersFetcher: () => dispatch(ordersFetcher("orders", "GET")),
   };
 };
 
