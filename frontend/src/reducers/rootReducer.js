bfb645f64a7566a3ac4efc3c1ad9cca46d944564
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-05-27 16:26:24 -0400

    [Frontend] Updates rootReducer.
    - Imports productReducer.
    - Adds products key that point to productReducer in combineReducers.

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import contentsReducer from "./contentsReducer";
 import productsReducer from "./productsReducer";
+import productReducer from "./productReducer";
 import shoppingCartReducer from "./shoppingCartReducer";
 import userReducer from "./userReducer";
 import ordersReducer from "./ordersReducer";
@@ -9,6 +10,7 @@ import orderReducer from "./orderReducer";
 const rootReducer = combineReducers({
   content: contentsReducer,
   products: productsReducer,
+  product: productReducer,
   shopping_cart: shoppingCartReducer,
   user: userReducer,
   orders: ordersReducer,
