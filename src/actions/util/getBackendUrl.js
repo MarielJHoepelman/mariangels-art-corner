695fbec4329e5507d65227025c82da5626f1f20e
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-08-05 14:24:33 -0400

    check remote env log

--- src/actions/util/getBackendUrl.js
+++ src/actions/util/getBackendUrl.js
@@ -1,4 +1,5 @@
 export const getBackendUrl = () => {
+  console.log("ENV", process.env.NODE_ENV);
   return process.env.NODE_ENV === "production"
     ? "https://mariangels-art-corner-backend.herokuapp.com/"
     : "http://localhost:5000/";
