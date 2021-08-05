e3ede449d2a6a09d22346d7f36897a6d0d1dff9f
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-08-05 17:27:12 -0400

    Adds condition to ArtCard to handle content with no image.

--- src/components/ArtCard.js
+++ src/components/ArtCard.js
@@ -2,7 +2,9 @@ import { ArtCardLink } from "../styles";
 
 export const ArtCard = ({ art }) => (
   <ArtCardLink to={`/art/${art.page_name}`} key={art.page_name}>
-    <img src={art.image_file.square} alt={art.title} />
+    {art?.image_file?.square && (
+      <img src={art.image_file.square} alt={art.title} />
+    )}
     <h3>{art.title}</h3>
   </ArtCardLink>
 );
