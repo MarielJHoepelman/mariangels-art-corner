a93184d69a49462e69a28ff939a42436851f58d9
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-08-06 13:42:17 -0400

    Adds conditional statement to handle reference to Redux DEVTOOLS only when client is using Chrome.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,10 @@ const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.navigator.userAgent.includes("Chrome")
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      : compose
   )
 );
 
