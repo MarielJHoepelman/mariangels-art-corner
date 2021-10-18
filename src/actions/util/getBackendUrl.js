2aa3277d9558a45553077c48ebfe9b2d21cf9f2f
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-10-18 18:08:17 -0400

    Removes console.log.

--- src/actions/util/getBackendUrl.js
+++ src/actions/util/getBackendUrl.js
@@ -1,5 +1,4 @@
 export const getBackendUrl = () => {
-  console.log("ENV", process.env.NODE_ENV);
   return process.env.NODE_ENV === "production"
     ? "https://mariangels-art-corner-backend.herokuapp.com/"
     : "http://localhost:5000/";
