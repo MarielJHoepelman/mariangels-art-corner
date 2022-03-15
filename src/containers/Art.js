86cc2670de05f0ad00b2ef227ce3371e67b78884
Mariel Hoepelman <marielhoepelman@gmail.com>
2022-03-15 12:46:11 -0400

    Deletes commented out code.

--- src/containers/Art.js
+++ src/containers/Art.js
@@ -33,30 +33,3 @@ const Art = () => {
 }
 
 export default Art;
-
-
-
-/*class Art extends Component {
-  componentDidMount() {
-    this.props.fetcher();
-  }
-
-  render() {
-    return (
-      <ArtComponent content={this.props.content} loading={this.props.loading} />
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return { content: state.content, loading: state.loading };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetcher: () => dispatch(fetcher("contents/art", "GET")),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Art);*/
-
