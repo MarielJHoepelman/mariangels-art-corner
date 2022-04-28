5a5fa6b9e9bfc6e28b269e3c6ad3422b58206768
Mariel Hoepelman <marielhoepelman@gmail.com>
2022-04-28 16:24:28 -0400

    Fixes dev tools Redux issue.

--- src/index.js
+++ src/index.js
@@ -7,16 +7,14 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.navigator.userAgent.includes("Chrome")
-      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
-      : compose
-  )
-);
+const middleware = [
+  applyMiddleware(thunk),
+  ...(window.__REDUX_DEVTOOLS_EXTENSION__
+    ? [window.__REDUX_DEVTOOLS_EXTENSION__()]
+    : []),
+];
+
+const store = createStore(rootReducer, middleware);
 
 ReactDOM.render(
   <Provider store={store}>
