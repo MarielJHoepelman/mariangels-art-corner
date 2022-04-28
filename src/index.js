ad0176200e77ada1e4f345a5b24e05b45b456592
Mariel Hoepelman <marielhoepelman@gmail.com>
2022-04-28 16:44:51 -0400

    Update packages.

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 
-const middleware = [
+const middleware = compose(
   applyMiddleware(thunk),
   ...(window.__REDUX_DEVTOOLS_EXTENSION__
     ? [window.__REDUX_DEVTOOLS_EXTENSION__()]
-    : []),
-];
+    : [])
+);
 
 const store = createStore(rootReducer, middleware);
 
